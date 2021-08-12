8b3f6b99518d482c6615748fbce10c883477e086
SirNoob97 <martinald467@gmail.com>
2021-08-12 22:19:03 +0200

    add java project

---
+++ backend/src/test/java/com/sirnoob97/github/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirnoob97.github.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
