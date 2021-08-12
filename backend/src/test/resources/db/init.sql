285c6d61b6e5278b0f8e67c297eb65b67f27c022
SirNoob97 <martinald467@gmail.com>
2021-08-12 23:58:20 +0200

    add init script

---
+++ backend/src/test/resources/db/init.sql
@@ -0,0 +1,17 @@
+CREATE TYPE animal AS ENUM (
+  'BEAR',
+  'CAT',
+  'DOG',
+  'ELEPHANT',
+  'GORILLA',
+  'HORSE',
+  'JAGUAR',
+  'KANGAROO',
+  'KOALA',
+  'LION',
+  'MONKEY',
+  'PANDA',
+  'PENGUIN',
+  'TIGER',
+  'ZEBRA'
+);
