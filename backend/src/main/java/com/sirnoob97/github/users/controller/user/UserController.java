b9ec56ffde663cf09b66a0c965fca0e48d3a08be
SirNoob97 <martinald467@gmail.com>
2021-08-14 00:39:49 +0200

    move and add method to find users by animal

---
+++ backend/src/main/java/com/sirnoob97/github/users/controller/user/UserController.java
@@ -0,0 +1,47 @@
+package com.sirnoob97.github.users.controller.user;
+
+import java.util.stream.Collectors;
+
+import com.sirnoob97.github.users.domain.animal.AnimalRepository;
+import com.sirnoob97.github.users.domain.user.UserRepository;
+import com.sirnoob97.github.users.dto.UserRequest;
+import com.sirnoob97.github.users.dto.UserResponse;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  private UserRepository userRepository;
+  private AnimalRepository animalRepository;
+
+  @PostMapping
+  public ResponseEntity<?> newUser(@RequestBody UserRequest userRequest) {
+    var animals = userRequest.getAnimals().stream()
+        .map(a -> animalRepository.findByName(a))
+        .map(o -> o.get())
+        .collect(Collectors.toSet());
+
+    userRepository.save(userRequest.mapToUser(animals));
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<UserResponse>> getUsers(@RequestParam(name = "animal") String name, Pageable page){
+    var animal = animalRepository.findByName(name).get();
+    return ResponseEntity.ok(userRepository.findByAnimalOrderByPoints(animal, page));
+  }
+}
