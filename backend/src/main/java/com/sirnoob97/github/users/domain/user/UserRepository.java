91bf8c53b4209f7745e8c5e8e451aab415ccb7f4
SirNoob97 <martinald467@gmail.com>
2021-08-14 17:23:07 +0200

    Add delete method and method of sorting users by points

--- backend/src/main/java/com/sirnoob97/github/users/domain/user/UserRepository.java
+++ backend/src/main/java/com/sirnoob97/github/users/domain/user/UserRepository.java
@@ -15,4 +15,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   @Query(value = "SELECT new com.sirnoob97.github.users.dto.UserResponse(u.userId, u.given, u.surname, u.points, u.age) FROM User u WHERE :animal MEMBER u.animals AND u.isActive = true ORDER BY u.points DESC")
   Page<UserResponse> findByAnimalOrderByPoints(@Param("animal") Animal animal, Pageable page);
+
+  @Query(value = "SELECT new com.sirnoob97.github.users.dto.UserResponse(u.userId, u.given, u.surname, u.points, u.age) FROM User u ORDER BY u.points DESC")
+  Page<UserResponse> findAllByOrderByPointsDesc(Pageable page);
+
+  void deleteByUserId(Long userId);
 }
