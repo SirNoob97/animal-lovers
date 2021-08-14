cbc8f0644e4fb231358846c4d36c09f90d758511
SirNoob97 <martinald467@gmail.com>
2021-08-14 16:19:21 +0200

    add userId variable

--- backend/src/main/java/com/sirnoob97/github/users/dto/UserResponse.java
+++ backend/src/main/java/com/sirnoob97/github/users/dto/UserResponse.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserResponse {
 
+  private Long userId;
   private String given;
   private String surname;
   private Integer points;
