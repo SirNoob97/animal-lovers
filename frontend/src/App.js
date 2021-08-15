5fb6d7393618e2d4d057fece5be68e5351e035ff
SirNoob97 <martinald467@gmail.com>
2021-08-15 12:29:54 +0200

    remove react default page, add user form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import React, { useState } from "react";
-import ReactPaginate from "react-paginate";
-
-import JsonData from "../../users.json";
+import FromContainer from './form/Container';
+import React from "react";
 
 function App() {
-  const [users, setUsers] = = useState(JsonData.slice(0, 101));
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FromContainer />
   );
 }
 
