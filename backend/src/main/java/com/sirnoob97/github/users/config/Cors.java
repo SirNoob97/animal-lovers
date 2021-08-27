07446972f3e9865dfebf4a3157ee83943ed49221
SirNoob97 <martinald467@gmail.com>
2021-08-27 15:57:51 +0200

    fix package name

--- backend/src/main/java/com/sirnoob97/github/users/config/Cors.java
+++ backend/src/main/java/com/sirnoob97/github/users/config/Cors.java
@@ -1,4 +1,4 @@
-package com.sirnoob97.github.users.cognfig;
+package com.sirnoob97.github.users.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
