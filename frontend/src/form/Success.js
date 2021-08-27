2b2861b83d3b3fea62bbdbdeb1d034c9e6ec608f
SirNoob97 <martinald467@gmail.com>
2021-08-27 21:43:04 +0200

    change confirmation

--- frontend/src/form/Success.js
+++ frontend/src/form/Success.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Success = () => {
   return (
     <div className='position-absolute top-50 start-50 translate-middle text-light'>
-      <h1>We have received your request!</h1>
+      <h1>User created successfully!</h1>
     </div>
   );
 };
