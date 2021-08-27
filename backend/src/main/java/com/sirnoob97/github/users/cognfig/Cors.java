ec78514e5f82d1bef4dec4ddf2ab09e22bfe32aa
SirNoob97 <martinald467@gmail.com>
2021-08-27 15:56:05 +0200

    add cors config for local client

---
+++ backend/src/main/java/com/sirnoob97/github/users/cognfig/Cors.java
@@ -0,0 +1,18 @@
+package com.sirnoob97.github.users.cognfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name())
+        .allowedHeaders("*")
+        .allowedOrigins("http://localhost:3000");
+  }
+}
