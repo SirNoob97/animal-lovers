51500896ac3af845cebd2cade9745ce2fdcc39d4
SirNoob97 <martinald467@gmail.com>
2021-08-28 20:01:13 +0200

    allow delete method

--- backend/src/main/java/com/sirnoob97/github/users/config/Cors.java
+++ backend/src/main/java/com/sirnoob97/github/users/config/Cors.java
@@ -11,7 +11,7 @@ public class Cors implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name())
+        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name())
         .allowedHeaders("*")
         .allowedOrigins("http://localhost:3000");
   }
