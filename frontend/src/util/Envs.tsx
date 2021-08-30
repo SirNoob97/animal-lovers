bd4e13e848f0910fc95b44cb4ec6f558bd4ace0a
SirNoob97 <martinald467@gmail.com>
2021-08-30 20:19:48 +0200

    add component and file to import env variables into project

---
+++ frontend/src/util/Envs.tsx
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({path : './../../..env.development'})
+
+interface Variables {
+  readonly DEFAULT_BACKEND_URL: string;
+  readonly BACKEND_URL: string;
+  readonly USERS_ENDPOINT: string;
+  readonly ANIMALS_ENDPOINT: string;
+}
+
+const Envs: Variables = {
+  DEFAULT_BACKEND_URL : `${process.env.REACT_APP_DEFAULT_BACKEND_URL}`,
+  BACKEND_URL : `${process.env.REACT_APP_BACKEND_URL}`,
+  USERS_ENDPOINT : `${process.env.REACT_APP_BACKEND_USERS_ENDPOINT}`,
+  ANIMALS_ENDPOINT : `${process.env.REACT_APP_BACKEND_ANIMALS_ENDPOINT}`
+}
+
+export default Envs;
