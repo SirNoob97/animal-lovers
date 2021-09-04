65179f9db930c3bc7c80ad508f37d3331ef75601
SirNoob97 <martinald467@gmail.com>
2021-09-04 20:44:17 +0200

    add client infog variables to allow the origins and format

--- backend/src/main/java/com/sirnoob97/github/users/config/Cors.java
+++ backend/src/main/java/com/sirnoob97/github/users/config/Cors.java
@@ -1,5 +1,6 @@
 package com.sirnoob97.github.users.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,16 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class Cors implements WebMvcConfigurer {
 
+  @Value(value = "${client.info.hostname}")
+  public String clientHostname;
+
+  @Value(value = "${client.info.port}")
+  public int clientPort;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry
-      .addMapping("/**")
-      .allowedMethods(
-        HttpMethod.GET.name(),
-        HttpMethod.POST.name(),
-        HttpMethod.DELETE.name()
-      )
-      .allowedHeaders("*")
-      .allowedOrigins("http://localhost:3000");
+        .addMapping("/**")
+        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name())
+        .allowedHeaders("*")
+        .allowedOrigins(
+            String.format("http://%s:%s", clientHostname, clientPort),
+            "http://localhost",
+            "http://localhost:3000");
   }
 }
